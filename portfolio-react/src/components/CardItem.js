9d58c42e735fa153efe17d0bdc06aae157a31fc2
nickdsaunders <>
2021-07-08 16:16:47 -0700

    made lots of updates pushing to test netlify

--- portfolio-react/src/components/CardItem.js
+++ portfolio-react/src/components/CardItem.js
@@ -1,24 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CardItem = (props) => {
   return (
     <>
       <li className="cards__item">
         <Link className="cards__item__link" to={props.path}>
-          <figure className="cards__item__pic-wrap" data-category={props.label}>
+          <figure className="cards__item__pic-wrap">
             <img
               src={props.src}
-              alt="Image Name"
+              alt="project-screenshot"
               className="cards__item__img"
             />
           </figure>
           <div className="cards__item__info">
             <h5 className="cards__item__head">{props.head}</h5>
+            <div className="card-head-divider"></div>
             <p className="cards__item__text">{props.text}</p>
-            <a href="https://github.com/nickdsaunders" target="_blank">
+            {/* <a href="https://github.com/nickdsaunders" target="_blank">
               <i className="fab fa-github" />
-            </a>
+            </a> */}
           </div>
         </Link>
       </li>
--- portfolio-react/src/components/Projects.js
+++ portfolio-react/src/components/Projects.js
@@ -1,54 +1,54 @@
-import React from "react";
-import CardItem from "./CardItem";
-import "../styles/Cards.css";
-import Water from "../images/water-photo.jpg";
+import React from 'react';
+import CardItem from './CardItem';
+import '../styles/Cards.css';
+import Water from '../images/water-photo.jpg';
+import ZigZag from '../images/zigzag.png';
+import Gif1 from '../images/giphy-test.gif';
 
 const Projects = () => {
   return (
-    <div className="cards">
-      <h1>Projects</h1>
-      <div className="cards__container">
-        <div className="cards__wrapper">
-          <ul className="cards__items">
-            <CardItem
-              src={Water}
-              head="Enter Project Name Here"
-              text="Enter project description here"
-              label="Project"
-              path="/"
-            />
-            <CardItem
-              src={Water}
-              head="Enter Project Name Here"
-              text="Enter project description here"
-              label="Project1"
-              path="/"
-            />
-          </ul>
-          <ul className="cards__items">
-            <CardItem
-              src={Water}
-              head="Enter Project Name Here"
-              text="Enter project description here"
-              label="Project1"
-              path="/"
-            />
-            <CardItem
-              src={Water}
-              head="Enter Project Name Here"
-              text="Enter project description here"
-              label="Project1"
-              path="/"
-            />
-            <CardItem
-              src={Water}
-              head="Enter Project Name Here"
-              text="Enter project description here"
-              label="Project1"
-              path="/"
-            />
-          </ul>
-        </div>
+    <div className="cards__container">
+      <img className="projects-zigzag" alt="decorative" src={ZigZag} />
+      <h1 className="projects-h1">Projects</h1>
+      <div className="divider-projects"></div>
+      <div className="cards__wrapper">
+        <ul className="cards__items">
+          <CardItem
+            src={Gif1}
+            head="Thirsty For Knowledge"
+            text="Thirsty For Knowledge is an exciting game of trivia built with React in Rails, where the user sets the category and difficulty at the start of the game to pull the trivia questions from an external API.  
+            Users gain or loose points based on their answer choice. The higher the difficulty the more points are awarded.
+            The final score is then recorded on the leaderboard table in the database.
+             Open web app | View repo"
+            path="/"
+          />
+          <CardItem
+            src={Water}
+            head="NuPad"
+            text="NuPad is an apartment finding app that was built using React in Rails and has full CRUD functionality creating, updating, and deleting apartment listings."
+            path="/"
+          />
+        </ul>
+        <ul className="cards__items">
+          <CardItem
+            src={Water}
+            head="Frisky Felines"
+            text="Frisky Felines, like Tinder but for Cats, is my first full stack application with a decoupled React frontend and Rails backend. It features full CRUD"
+            path="/"
+          />
+          <CardItem
+            src={Water}
+            head="Portfolio"
+            text="Enter project description here"
+            path="/"
+          />
+          <CardItem
+            src={Water}
+            head="Webstacks Dashboard"
+            text="Enter project description here"
+            path="/"
+          />
+        </ul>
       </div>
     </div>
   );
